0035fb448d460687e05add1b9aeb019c55271141
BlackT1221 <cdmartinez1121@gmail.com>
2026-05-26 09:04:39 -0500

    Cambios al inventario

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def get_repuestos():
 	  "status": "online",
 	  "servidor": "Ubuntu de Martinez:",
 	  "hora_servidor": str(datetime.datetime.now()),
-	  "inventario": ["Bujias de Iridio", "Filtro de aceite", "Aceite motul 7100"]
+	  "inventario": ["Bujias de Iridio", "Filtro de aceite", "Aceite motul 7100", "Pastillas de freno", "Amortiguadores", "Correa de distribucion"]
 	})
 
 if __name__ == "__main__":
